558328faedbe796a40f8ff2807b5924e804fd3ab
jupercy-sant <juper.sant@gmail.com>
2024-06-17 10:05:26 -0300

    adiciona mudanças

---
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+// ResultadoEscolar.java
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+       int nota = 3;
+
+       String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "recuperação" : "Reprovado";
+
+       System.out.println(resultado);
+    }
+}
